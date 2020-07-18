b150427b807711376d210b614674b56968c2b9e6
rezaizadijahromi <rezaizadij2000@gmail.com>
2020-07-18 13:51:36 +0430

    creating urls

---
+++ src/core_new_v/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+
+from . import views
+
+app_name = 'core_new_v'
+
+urlpatterns = [
+    path('', views.Home.as_view(), name='home'),
+    path('add_question/', views.AddQ.as_view(), name='add_question'),
+    path('question/<slug:slug>/', views.QuetionView, name='question'),
+    # path('question_tedad/<slug:slug>/', views.add_request_quantity, name='add_request'),
+    path('question/<slug:slug>/answer/', views.AddAnswerToQuestion, name='add_answer'),
+    # path('question/<slug:slug>/answer/<int:id>/', views.AnswerDetail, name='answer_detail'),
+    # path('answer/<int:id>/', views.LikeVote, name='add_vote'),
+    # path('delete/<int:id>/', views.DislikeVote, name='remove_vote'),
+
+]
